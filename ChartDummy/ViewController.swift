17e98d7ac793a4dbf65f1e70f9ea46a6725aa240
Annicha <annicha.dev@gmail.com>
2019-07-08 15:45:11 -0600

    Initial Commit

---
+++ ChartDummy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChartDummy
+//
+//  Created by Annicha Hanwilai on 7/8/19.
+//  Copyright © 2019 Annicha Hanwilai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
